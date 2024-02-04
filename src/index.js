bc833072ebcf5315b31599047dd5c83a9e01da75
Yevhenii <yevhenii.kul@gmail.com>
2024-02-04 16:13:05 +0100

    Class component for pixabayApi has been created

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
 import './index.css';
+import { PixabayApi } from 'components/api/pixabayApi';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <PixabayApi></PixabayApi>
   </React.StrictMode>
 );
